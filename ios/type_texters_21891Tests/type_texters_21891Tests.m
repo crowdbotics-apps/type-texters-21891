04185f05bedbba32d62a8ecb3b071db636d64fdc
Crowdbotics <team@crowdbotics.com>
2020-10-23 19:31:31 +0000

    Initial commit.

---
+++ ios/type_texters_21891Tests/type_texters_21891Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface type_texters_21891 : XCTestCase
+
+@end
+
+@implementation type_texters_21891
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
